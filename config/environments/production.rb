8aa16aeb874bafe817daaf9f0d8145f142a26720
luolisa <lisa@dali.dartmouth.edu>
2014-02-24 17:12:14 +0800

    prodding around

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,7 +20,8 @@ Lisaluo::Application.configure do
   # config.action_dispatch.rack_cache = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this).
-  config.serve_static_assets = false
+  # CHANGED TO TRUE 2/24/14
+  config.serve_static_assets = true
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
