6b6d57fce8df2095ce792c482a7cde99d4fdc52a
luolisa <lisa@dali.dartmouth.edu>
2014-08-26 00:58:02 -0400

    about page added

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Lisaluo::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
   # You can have the root of your site routed with "root"
   root 'home#index'
-  
+  resources 'about'
   resources 'portfolio'
   resources 'projects'
   resources 'inspiration'
