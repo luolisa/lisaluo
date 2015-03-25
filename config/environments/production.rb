40efb90e30940c93156c63ba6c9d48c3b6d0fc3b
luoser <lisa@dali.dartmouth.edu>
2015-03-24 22:25:14 -0400

    assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -30,7 +30,7 @@ Lisaluo::Application.configure do
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   # CHANGED to TRUE 14/2/23
   # this might make things slow...
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Generate digests for assets URLs.
   config.assets.digest = true
