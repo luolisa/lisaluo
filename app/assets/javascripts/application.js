3e06de2408cccdc02d8f6e58b9f15e237bc39ca3
Lisa Luo <lluo92@gmail.com>
2016-10-26 23:56:10 -0400

    update configs n stuff to 2016

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,6 +13,7 @@
 //= require jquery
 //= require jquery.turbolinks
 //= require jquery_ujs
+//= require twitter/bootstrap
 //= require masonry.pkgd.min.js
 //= require imagesloaded.pkgd.js
 //= require turbolinks
